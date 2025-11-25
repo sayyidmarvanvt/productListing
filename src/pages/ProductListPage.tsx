72c3dda1c900b58ae148e4d5467a8c80aa4cb74d
Sayyid Marvan <sayyidmarvanvt@gmail.com>
2025-11-25 11:38:36 +0530

    fixed filterd error

--- src/pages/ProductListPage.tsx
+++ src/pages/ProductListPage.tsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo } from "react";
 import { useNavigate } from "react-router-dom";
+
 interface Data {
   id: number;
   title: string;
@@ -13,44 +14,34 @@ interface Data {
 function ProductListPage() {
   const [data, setData] = useState<Data[]>([]);
   const [searchField, setSearchField] = useState("");
-  const [filteredData, setFilteredData] = useState<Data[]>([]);
   const [selectedOption, setSelectedOption] = useState<string>("low to high");
 
   const navigate = useNavigate();
 
   const onSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const searchString = event.target.value.toLocaleLowerCase();
-    setSearchField(searchString);
+    setSearchField(event.target.value.toLowerCase());
   };
-  useEffect(() => {
-    fetch("https://fakestoreapi.com/products").then((response) =>
-      response.json().then((users) => setData(users))
-    );
-  }, []);
 
   useEffect(() => {
-    const filteredData = data
-      .sort((a, b) => {
-        if (selectedOption === "high to low") {
-          return b.price - a.price;
-        } else {
-          return a.price - b.price;
-        }
-      })
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((products) => setData(products));
+  }, []);
+
+  const filteredData = useMemo(() => {
+    return [...data]
+      .sort((a, b) =>
+        selectedOption === "high to low" ? b.price - a.price : a.price - b.price
+      )
       .filter((item) =>
         item.title.toLowerCase().includes(searchField.toLowerCase())
       );
-
-    setFilteredData(filteredData);
   }, [data, selectedOption, searchField]);
 
   const handleOptionChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     setSelectedOption(event.target.value);
   };
 
-  const handleViewDetails = (productId: string) => {
-    navigate(`/products/${productId}`);
-  };
   return (
     <>
       <input type="search" placeholder="search" onChange={onSearchChange} />
@@ -58,13 +49,14 @@ function ProductListPage() {
         <option value="low to high">Low to High</option>
         <option value="high to low">High to Low</option>
       </select>
+
       <div className="container">
         {filteredData.map((item) => (
           <div className="card" key={item.id}>
             <img src={item.image} alt={item.title} className="card-img" />
             <p>{item.title}</p>
             <p>{item.price}</p>
-            <button onClick={() => handleViewDetails(item.id)}>
+            <button onClick={() => navigate(`/products/${item.id}`)}>
               View Details
             </button>
           </div>
